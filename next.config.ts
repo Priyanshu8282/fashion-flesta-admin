9ecd7caee4b0393cb58be3cf1bc84babc0e08d3a
Priyanshu8282 <priyanshus8282@gmail.com>
2026-03-04 16:22:57 +0530

    Add image remote patterns configuration for Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fashion-flesta-backend-91rm.onrender.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
